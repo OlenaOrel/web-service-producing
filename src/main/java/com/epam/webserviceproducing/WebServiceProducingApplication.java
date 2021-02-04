67e6cd3b955d84246b5661689eca5c759deb783f
Olena Orel <olena_orel@epam.com>
2021-02-04 18:24:52 +0200

    01 - Initial commit

---
+++ src/main/java/com/epam/webserviceproducing/WebServiceProducingApplication.java
@@ -0,0 +1,13 @@
+package com.epam.webserviceproducing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceProducingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceProducingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/webserviceproducing/WebServiceProducingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.webserviceproducing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceProducingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
